da7dac913561fe41c0e2763f4d048975cfa090f3
Jan Sanchez <joejansanchez@gmail.com>
2016-07-19 00:15:55 -0500

    feat(babel): agrego babel al proyecto

---
+++ bitbucket-payload-reader.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+import request from 'request';
+
+const url = 'http://www.json-generator.com/api/json/get/bVXFCsEbDm?indent=2';
+
+request(url, (error, response, body) => {
+	if (!error && response.statusCode == 200) {
+		console.log(body);
+	}
+});
